7c663419c4ac78ff2a63d230c1332b5adaca6909
Williano <paawilly17@gmail.com>
2020-03-27 00:03:58 -0500

    Add bottom navigationbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
+  int _currentTab = 0;
 
   List<IconData> _icons = [
     FontAwesomeIcons.plane,
@@ -75,6 +76,34 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentTab,
+        onTap: (int value) {
+          setState(() {
+            _currentTab = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                size: 30.0,
+              ),
+              title: SizedBox.shrink()),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.local_pizza,
+                size: 30.0,
+              ),
+              title: SizedBox.shrink()),
+          BottomNavigationBarItem(
+              icon: CircleAvatar(
+                radius: 15.0,
+                backgroundImage: NetworkImage("http://i.imgur.com/zL4Krbz.jpg"),
+              ),
+              title: SizedBox.shrink())
+        ],
       ),
     );
   }
