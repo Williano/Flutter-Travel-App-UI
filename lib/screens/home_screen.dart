94c372ee7cedb0b093d223100ed9885dbfe7ea72
Williano <paawilly17@gmail.com>
2020-03-26 21:20:13 -0500

    Format code

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,7 +8,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   int _selectedIndex = 0;
 
   List<IconData> _icons = [
@@ -18,31 +17,32 @@ class _HomeScreenState extends State<HomeScreen> {
     FontAwesomeIcons.biking,
   ];
 
-  Widget _buildIcon(int index)
-  {
+  Widget _buildIcon(int index) {
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         setState(() {
           _selectedIndex = index;
         });
       },
-        child: Container(
-          height: 60.0,
-          width: 60.9,
-          decoration: BoxDecoration(
-            color: _selectedIndex == index ?
-            Theme.of(context).accentColor : Color(0xFFE7EBEE),
-            borderRadius: BorderRadius.circular(30.0),
+      child: Container(
+        height: 60.0,
+        width: 60.9,
+        decoration: BoxDecoration(
+          color: _selectedIndex == index
+              ? Theme.of(context).accentColor
+              : Color(0xFFE7EBEE),
+          borderRadius: BorderRadius.circular(30.0),
         ),
         child: Icon(
           _icons[index],
           size: 25.0,
-          color: _selectedIndex == index ? Theme.of(context).primaryColor : Color(0xFFB4C1C4),
-          ),
+          color: _selectedIndex == index
+              ? Theme.of(context).primaryColor
+              : Color(0xFFB4C1C4),
         ),
+      ),
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -52,24 +52,25 @@ class _HomeScreenState extends State<HomeScreen> {
           padding: const EdgeInsets.symmetric(vertical: 30.0),
           child: ListView(
             children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0, right:120.0),
-              child: Text(
-                "What would you like to find?",
-                style: TextStyle(
-                fontSize: 30.0,
-                fontWeight:FontWeight.bold),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 120.0),
+                child: Text(
+                  "What would you like to find?",
+                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: _icons.asMap().entries.map(
-                (MapEntry map) => _buildIcon(map.key)).toList()
-            ),
-            SizedBox(height: 20.0),
-            DestinationCarousel(),
-          ],),
+              SizedBox(height: 20.0),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: _icons
+                      .asMap()
+                      .entries
+                      .map((MapEntry map) => _buildIcon(map.key))
+                      .toList()),
+              SizedBox(height: 20.0),
+              DestinationCarousel(),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/hotel_carousel.dart
+++ lib/widgets/hotel_carousel.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import '../models/destination_model.dart';
 
-class DestinationCarousel extends StatelessWidget {
+class HotelCarousel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
