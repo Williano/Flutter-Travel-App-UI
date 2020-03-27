00c811662055320c9baffe78aff100eb46818a22
Williano <paawilly17@gmail.com>
2020-03-26 21:44:20 -0500

    Add Hotel carousel

--- lib/widgets/hotel_carousel.dart
+++ lib/widgets/hotel_carousel.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-import '../models/destination_model.dart';
+import '../models/hotel_model.dart';
 
 class HotelCarousel extends StatelessWidget {
   @override
@@ -14,7 +12,7 @@ class HotelCarousel extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Text(
-                "Top Destinations",
+                "Exclusive Hotels",
                 style: TextStyle(
                     fontSize: 22.0,
                     fontWeight: FontWeight.bold,
@@ -36,12 +34,12 @@ class HotelCarousel extends StatelessWidget {
           height: 300.0,
           child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemCount: destinations.length,
+              itemCount: hotels.length,
               itemBuilder: (BuildContext context, int index) {
-                Destination destination = destinations[index];
+                Hotel hotel = hotels[index];
                 return Container(
                   margin: EdgeInsets.all(10.0),
-                  width: 210.0,
+                  width: 240.0,
                   child: Stack(
                     alignment: Alignment.topCenter,
                     children: <Widget>[
@@ -49,7 +47,7 @@ class HotelCarousel extends StatelessWidget {
                         bottom: 15.0,
                         child: Container(
                           height: 120.0,
-                          width: 200,
+                          width: 240,
                           decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(10.0),
@@ -58,19 +56,24 @@ class HotelCarousel extends StatelessWidget {
                             padding: const EdgeInsets.all(10.0),
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.end,
-                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
                                 Text(
-                                  "${destination.activities.length} activities",
+                                  hotel.name,
                                   style: TextStyle(
                                       fontSize: 22.0,
                                       fontWeight: FontWeight.w600,
                                       letterSpacing: 1.2),
                                 ),
+                                SizedBox(height: 2.0),
                                 Text(
-                                  destination.description,
+                                  hotel.address,
                                   style: TextStyle(color: Colors.grey),
-                                )
+                                ),
+                                SizedBox(height: 2.0),
+                                Text("\$${hotel.price} / night",
+                                    style: TextStyle(
+                                        fontSize: 14.0,
+                                        fontWeight: FontWeight.w600))
                               ],
                             ),
                           ),
@@ -86,47 +89,14 @@ class HotelCarousel extends StatelessWidget {
                                   offset: Offset(0.0, 2.0),
                                   blurRadius: 6.0)
                             ]),
-                        child: Stack(
-                          children: <Widget>[
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(20.0),
-                              child: Image(
-                                height: 180.0,
-                                width: 180.0,
-                                image: AssetImage(destination.imageUrl),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Positioned(
-                              left: 10.0,
-                              bottom: 10.0,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    destination.city,
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 24.0,
-                                        fontWeight: FontWeight.w600,
-                                        letterSpacing: 1.2),
-                                  ),
-                                  Row(children: <Widget>[
-                                    Icon(
-                                      FontAwesomeIcons.locationArrow,
-                                      size: 10.0,
-                                      color: Colors.white,
-                                    ),
-                                    SizedBox(width: 5.0),
-                                    Text(
-                                      destination.country,
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  ])
-                                ],
-                              ),
-                            )
-                          ],
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20.0),
+                          child: Image(
+                            height: 180.0,
+                            width: 220.0,
+                            image: AssetImage(hotel.imageUrl),
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       )
                     ],
